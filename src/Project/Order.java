9b914bb50bf34568b5cd48cce53f5a841969543f
Fnony <fnonh100@gamil.com>
2025-02-27 19:21:16 +0300

    new uptade afnan

--- src/Project/Order.java
+++ src/Project/Order.java
@@ -1,5 +1,78 @@
 package Project;
 
 public class Order {
+private int orderID;
+private double totalPrice;
+private BakeryItem[] itemList;
+private int itemCounter;
 
+
+public Order(int id) {
+	orderID=id;
+	itemList=new BakeryItem[10];
+	itemCounter=0;
+	totalPrice=0.0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//end of class Order
